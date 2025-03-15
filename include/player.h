45a682e80635f901c9d4258d040d230d5a514009
raphael <raphael@localhost.localdomain>
2025-03-15 18:22:44 +0100

    Added player first functions

--- include/player.h
+++ include/player.h
@@ -3,6 +3,14 @@
 
 #include <stdbool.h>
 
+#define LEFT 1
+#define RIGHT 2
+#define UP 3
+#define DOWN 4
+
+#define DEFAULT_CELL_SIZE 10.0
+#define LEFT_MAP_X 0.0
+#define RIGHT_MAP_X 100.0
 
 typedef struct _Player {
     // Skin *skin;
@@ -10,7 +18,16 @@ typedef struct _Player {
     int direction; //macro
     float h_position;
     bool is_jumping;
+    float grid_cell_width;
 
 } Player;
+
+Player *player_make(float grid_cell_size, float h_position, int direction, bool alive, bool isJumping);
+
+Player *player_start();
+
+void player_free(Player *player);
+
+void move_player(int direction, Player *player);
 
 #endif
---
+++ src/player.c
@@ -0,0 +1,99 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "player.h"
+
+
+/**
+ * Crée un pointeur vers un nouveau joueur
+ * initialisé avec les paramètres donnés.
+ * Cette structure représente le personnage
+ * contrôlé par le joueur
+ * La structure est allouée sur le tas.
+ * 
+ * @author : Raphaël
+ * @pre : 0 <= h_position < fin_de_la_map
+ */
+Player *player_make(float grid_cell_size, float h_position, int direction, bool alive, bool isJumping)
+{
+    Player *ans = malloc(sizeof(Player));
+    ans->grid_cell_width = grid_cell_size;
+    ans->h_position = h_position;
+    ans->direction = direction;
+    ans->alive = alive;
+    ans->is_jumping = isJumping;
+    return ans;
+}
+
+/**
+ * Crée un nouveau personnage au joueur initialisé
+ * avec les paramètres nécessaires au début de la partie.
+ * 
+ * @author : Raphaël
+ */
+Player *player_start()
+{
+    return player_make(DEFAULT_CELL_SIZE, 0.0, UP, true, false);
+}
+
+/**
+ * Libère la mémoire associée à l'entité player.
+ * 
+ * @param[in] player L'entité dont on veut libérer la mémoire
+ * 
+ * @author : Raphaël
+ */
+void player_free(Player *player)
+{
+    free(player);
+}
+
+/**
+ * Change les attributs du personnage en fonction du mouvement.
+ * On suppose que le mouvement est valide. Le comportement peut-
+ * être partiellement indéfini.
+ * 
+ * @param[in] direction Direction du mouvement
+ * @param[in] player l'entité concernée par le mouvement valide
+ * 
+ * @author : Raphaël
+ */
+void move_player(int direction, Player *player)
+{
+    switch (direction)
+    {
+    case LEFT:
+        if (player->h_position - player->grid_cell_width < LEFT_MAP_X)
+        {
+            printf("Invalid move: direction %s\n", "left");
+        }
+        else
+        {
+            player->h_position -= player->grid_cell_width;
+        }
+        player->direction = LEFT;
+        break;
+
+    case RIGHT:
+        if (player->h_position + player->grid_cell_width > RIGHT_MAP_X)
+        {
+            printf("Invalid move: direction %s\n", "right");
+        }
+        else
+        {
+            player->h_position += player->grid_cell_width;
+        }
+        player->direction = RIGHT;
+        break;
+
+    case UP:
+        player->direction = UP;
+        break;
+
+    case DOWN:
+        player->direction = DOWN;
+        break;
+    
+    default:
+        printf("Invalid input : direction invalide : %d\n", direction);
+    }
+}
